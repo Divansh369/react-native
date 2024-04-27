4cad55d12b0fc5387755920359565971b0146c9e
Divansh369 <divanshthebest@gmail.com>
2024-04-27 13:11:19 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to</Text>
+      <Text style={[styles.text, styles.highlight]}>My Awesome App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  highlight: {
+    fontSize: 36,
+    textTransform: 'uppercase',
+  },
+});
+
